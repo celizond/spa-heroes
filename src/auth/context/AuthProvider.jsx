775a64a88a39f5826ba93862c0a634ccb14119eb
celizond <elizondo.celular@gmail.com>
2025-07-06 13:28:33 -0300

    login/logout base

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,40 @@
+import { useReducer } from "react";
+import { AuthContext } from "./AuthContext";
+import { authReducer } from "./authReducer";
+import { types } from "../types/types";
+
+const initialState = {
+    logged: false,
+}
+
+export const AuthProvider = ({ children }) => {
+
+    const [authState, dispatch] = useReducer(authReducer, initialState);
+
+    const login = (name = '') => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: '123',
+                name: name,
+            }
+        }
+        dispatch(action);
+    }
+
+    const logout = () => {
+        const action = {
+            type: types.logout,
+            payload: {
+                name: null,
+            }
+        }
+        dispatch(action);
+    }
+
+    return (
+        <AuthContext.Provider value={{...authState, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
